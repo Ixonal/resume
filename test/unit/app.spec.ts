07662f65a9b23a7ffebfa9d92e4703ccf0ae711a
Benjamin McGregor <ixonal@gmail.com>
2019-08-11 02:49:23 -0400

    Removing Invalid Tests
    There are some invalid tests build for the basic skeleton app that
    needed to be removed...

--- test/unit/app.spec.ts
+++ test/unit/app.spec.ts
@@ -1,26 +1,31 @@
-import {bootstrap} from 'aurelia-bootstrapper';
-import {StageComponent} from 'aurelia-testing';
-import {PLATFORM} from 'aurelia-pal';
+// import {bootstrap} from 'aurelia-bootstrapper';
+// import {StageComponent} from 'aurelia-testing';
+// import {PLATFORM} from 'aurelia-pal';
+import { App } from "../../src/app";
 
-describe('Stage App Component', () => {
-  let component;
+describe('App Component', () => {
+  // let component;
 
-  beforeEach(() => {
-    component = StageComponent
-      .withResources(PLATFORM.moduleName('app'))
-      .inView('<app></app>');
+  // beforeEach(() => {
+  //   component = StageComponent
+  //     .withResources(PLATFORM.moduleName('app'))
+  //     .inView('<app></app>');
+  // });
+
+  // afterEach(() => component.dispose());
+
+  it("should exist", () => {
+    expect(App).toBeTruthy();
   });
 
-  afterEach(() => component.dispose());
-
-  it('should render message', done => {
-    component.create(bootstrap).then(() => {
-      const view = component.element;
-      expect(view.textContent.trim()).toBe('Hello World!');
-      done();
-    }).catch(e => {
-      fail(e);
-      done();
-    });
-  });
+  // it('should render message', done => {
+  //   component.create(bootstrap).then(() => {
+  //     const view = component.element;
+  //     expect(view.textContent.trim()).toBe('Hello World!');
+  //     done();
+  //   }).catch(e => {
+  //     fail(e);
+  //     done();
+  //   });
+  // });
 });
